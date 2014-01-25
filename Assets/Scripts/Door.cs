4704a0aa0d3ad012ca2fc7a92dd71f3e55135ace
awSweeney <asweeney107@gmail.com>
2014-01-25 12:25:49 -0500

    Door things
    SUPPP

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	[SerializeField]
+	Shape _doorShape;
+
+	// Update is called once per frame
+	public Shape doorShape{
+		get{
+			return _doorShape;
+		}
+
+		set{
+			_doorShape = value;
+		}
+	}	
+}
---
+++ Assets/Scripts/DoorListing.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DoorListing : MonoBehaviour {
+
+	public List<GameObject> doorList;
+
+	// Use this for initialization
+	void Start () {
+		UpdateList();
+	}
+	
+	public void UpdateList(){
+		foreach(GameObject door in GameObject.FindGameObjectsWithTag ("Door")){
+			doorList.Add(door);
+		}
+	}
+
+	void ShapeChanged(){
+		UpdateList();
+	}
+}
