136884c2689876d5e4ac2c0fde3a5c5f6e951955
cslotterback <cslotterback@gmail.com>
2014-01-25 15:52:08 -0800

    skybox
    uploading skybox

---
+++ Assets/Scripts/TextBehavior.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextBehavior : MonoBehaviour {
+
+	[SerializeField]
+	private bool isQuit = false;
+
+	void Start(){
+
+	}
+
+	void OnMouseEnter(){
+		Debug.Log ("text entered");
+		renderer.material.color = Color.grey;
+
+	}
+
+	void OnMouseExit(){
+		Debug.Log ("text exited");
+		renderer.material.color = Color.white;
+
+	}
+
+	void OnMouseUp(){
+		Debug.Log ("text clicked");
+		if (isQuit == true) {
+			Application.Quit ();
+		} else {
+			Application.LoadLevel ("TestLevel");
+		}
+	}
+}
